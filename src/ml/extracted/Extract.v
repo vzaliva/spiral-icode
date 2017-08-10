fbe9d36fa429b849b3ce8f098585ce7b5b41c46f
Vadim Zaliva <lord@crocodile.org>
2017-08-10 14:42:29 -0700

    tweaking extraction

--- src/ml/extracted/Extract.v
+++ src/ml/extracted/Extract.v
@@ -1,6 +1,11 @@
 Require Icode.IAst.
 
+Require ExtrOcamlBasic.
+Require ExtrOcamlString.
+Require ExtrOcamlIntConv.
+
 Extraction Language Ocaml.
+Extraction Blacklist String List.
 
 (* OCaml pervasive types ---------------------------------------------------- *)
 (* Extract Inlined Constant Ollvm_ast.int => "int". *)
@@ -11,4 +16,6 @@ Set Extraction AccessOpaque.
 (* NOTE: assumes that this file is compiled from /src *)
 Cd "ml/extracted".
 
-Extraction Library IAst.
+Extraction Library ExtrOcamlIntConv.
+
+Recursive Extraction Library IAst.
