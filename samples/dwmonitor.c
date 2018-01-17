17221bfdf2e89d42b24e7920143355447e697832
Vadim Zaliva <lord@crocodile.org>
2018-01-17 14:40:34 -0800

    dwmonitor.c which compiles with gcc.

--- samples/dwmonitor.c
+++ samples/dwmonitor.c
@@ -27,49 +27,49 @@
 #include <float.h>
 
 int dwmonitor(float  *X, double  *D) {
-    __m128d u10, u11, u12, u13, u14, u15, u16, u9
-            , x20, x21, x22, x23, x25, x26, x27, x28
-            , x29, x32, x33, x36, x37, x38;
-    int w3;
+    __m128d u1, u2, u3, u4, u5, u6, u7, u8
+            , x1, x10, x13, x14, x17, x18, x19, x2
+            , x3, x4, x6, x7, x8, x9;
+    int w1;
     {
         unsigned _xm = _mm_getcsr();
         _mm_setcsr(_xm & 0xffff0000 | 0x0000dfc0);
-        u13 = _mm_set1_pd(0.0);
-        u10 = _mm_cvtps_pd(_mm_addsub_ps(_mm_set1_ps(FLT_MIN), _mm_set1_ps(X[0])));
-        u9 = _mm_set_pd(1.0, (-1.0));
-        for(int i33 = 0; i33 <= 2; i33++) {
-            x25 = _mm_addsub_pd(_mm_set1_pd((DBL_MIN + DBL_MIN)), _mm_loaddup_pd(&(D[i33])));
-            x20 = _mm_addsub_pd(_mm_set1_pd(0.0), u9);
-            x21 = _mm_mul_pd(x20, x25);
-            x22 = _mm_mul_pd(_mm_shuffle_pd(x20, x20, _MM_SHUFFLE2(0, 1)), x25);
-            x23 = _mm_sub_pd(_mm_set1_pd(0.0), _mm_min_pd(x21, x22));
-            u11 = _mm_add_pd(max(_mm_shuffle_pd(x23, x23, _MM_SHUFFLE2(0, 1)),x21), _mm_set1_pd(DBL_MIN));
-            u13 = _mm_add_pd(u13, u11);
-            x26 = _mm_addsub_pd(_mm_set1_pd(0.0), u9);
-            x27 = _mm_mul_pd(x26, u10);
-            x28 = _mm_mul_pd(_mm_shuffle_pd(x26, x26, _MM_SHUFFLE2(0, 1)), u10);
-            x29 = _mm_sub_pd(_mm_set1_pd(0.0), _mm_min_pd(x27, x28));
-            u9 = _mm_add_pd(max(_mm_shuffle_pd(x29, x29, _MM_SHUFFLE2(0, 1)),x27), _mm_set1_pd(DBL_MIN));
+        u5 = _mm_set1_pd(0.0);
+        u2 = _mm_cvtps_pd(_mm_addsub_ps(_mm_set1_ps(FLT_MIN), _mm_set1_ps(X[0])));
+        u1 = _mm_set_pd(1.0, (-1.0));
+        for(int i5 = 0; i5 <= 2; i5++) {
+            x6 = _mm_addsub_pd(_mm_set1_pd((DBL_MIN + DBL_MIN)), _mm_loaddup_pd(&(D[i5])));
+            x1 = _mm_addsub_pd(_mm_set1_pd(0.0), u1);
+            x2 = _mm_mul_pd(x1, x6);
+            x3 = _mm_mul_pd(_mm_shuffle_pd(x1, x1, _MM_SHUFFLE2(0, 1)), x6);
+            x4 = _mm_sub_pd(_mm_set1_pd(0.0), _mm_min_pd(x3, x2));
+            u3 = _mm_add_pd(_mm_max_pd(_mm_shuffle_pd(x4, x4, _MM_SHUFFLE2(0, 1)), _mm_max_pd(x3, x2)), _mm_set1_pd(DBL_MIN));
+            u5 = _mm_add_pd(u5, u3);
+            x7 = _mm_addsub_pd(_mm_set1_pd(0.0), u1);
+            x8 = _mm_mul_pd(x7, u2);
+            x9 = _mm_mul_pd(_mm_shuffle_pd(x7, x7, _MM_SHUFFLE2(0, 1)), u2);
+            x10 = _mm_sub_pd(_mm_set1_pd(0.0), _mm_min_pd(x9, x8));
+            u1 = _mm_add_pd(_mm_max_pd(_mm_shuffle_pd(x10, x10, _MM_SHUFFLE2(0, 1)), _mm_max_pd(x9, x8)), _mm_set1_pd(DBL_MIN));
         }
-        u14 = _mm_set1_pd(0.0);
-        for(int i31 = 0; i31 <= 1; i31++) {
-            u16 = _mm_cvtps_pd(_mm_addsub_ps(_mm_set1_ps(FLT_MIN), _mm_set1_ps(X[(i31 + 1)])));
-            u15 = _mm_cvtps_pd(_mm_addsub_ps(_mm_set1_ps(FLT_MIN), _mm_set1_ps(X[(3 + i31)])));
-            x33 = _mm_add_pd(u16, _mm_shuffle_pd(u15, u15, _MM_SHUFFLE2(0, 1)));
-            x32 = _mm_shuffle_pd(x33, x33, _MM_SHUFFLE2(0, 1));
-            u12 = _mm_shuffle_pd(_mm_min_pd(x32, x33), max(x32,x33), _MM_SHUFFLE2(1, 0));
-            u14 = _mm_shuffle_pd(_mm_min_pd(u12, u14), max(u12,u14), _MM_SHUFFLE2(1, 0));
+        u6 = _mm_set1_pd(0.0);
+        for(int i3 = 0; i3 <= 1; i3++) {
+            u8 = _mm_cvtps_pd(_mm_addsub_ps(_mm_set1_ps(FLT_MIN), _mm_set1_ps(X[(i3 + 1)])));
+            u7 = _mm_cvtps_pd(_mm_addsub_ps(_mm_set1_ps(FLT_MIN), _mm_set1_ps(X[(3 + i3)])));
+            x14 = _mm_add_pd(u8, _mm_shuffle_pd(u7, u7, _MM_SHUFFLE2(0, 1)));
+            x13 = _mm_shuffle_pd(x14, x14, _MM_SHUFFLE2(0, 1));
+            u4 = _mm_shuffle_pd(_mm_min_pd(x14, x13), _mm_max_pd(x14, x13), _MM_SHUFFLE2(1, 0));
+            u6 = _mm_shuffle_pd(_mm_min_pd(u6, u4), _mm_max_pd(u6, u4), _MM_SHUFFLE2(1, 0));
         }
-        x36 = _mm_addsub_pd(_mm_set1_pd(0.0), u14);
-        x37 = _mm_addsub_pd(_mm_set1_pd(0.0), u13);
-        x38 = _mm_cmpge_pd(x36, _mm_shuffle_pd(x37, x37, _MM_SHUFFLE2(0, 1)));
-        w3 = (_mm_testc_si128(_mm_castpd_si128(x38), _mm_set_epi32(0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)) - (_mm_testnzc_si128(_mm_castpd_si128(x38), _mm_set_epi32(0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff))));
-        // BASIC BLOCK BARRIER
+        x17 = _mm_addsub_pd(_mm_set1_pd(0.0), u6);
+        x18 = _mm_addsub_pd(_mm_set1_pd(0.0), u5);
+        x19 = _mm_cmpge_pd(x17, _mm_shuffle_pd(x18, x18, _MM_SHUFFLE2(0, 1)));
+        w1 = (_mm_testc_si128(_mm_castpd_si128(x19), _mm_set_epi32(0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff)) - (_mm_testnzc_si128(_mm_castpd_si128(x19), _mm_set_epi32(0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff))));
+        /* __asm nop; */
         if (_mm_getcsr() & 0x0d) {
             _mm_setcsr(_xm);
             return -1;
         }
         _mm_setcsr(_xm);
     }
-    return w3;
+    return w1;
 }
